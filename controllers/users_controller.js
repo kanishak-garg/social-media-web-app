4390a9d066b19da8275d6aacb43397d2358e5aa1
kanishak garg <kanishakgarg8778@gmail.com>
2021-05-06 00:46:54 +0530

    create the login signup form and route them

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,19 @@
 module.exports.profile = function (req, res) {
     return res.render('home', {
-        title: "world"
+        title: "Profile"
     });
-}
+};
+
+
+module.exports.sign_up = function (req, res) {
+    return res.render('sign_up', {
+        title: "sign_up"
+    });
+};
+
+
+module.exports.sign_in = function (req, res) {
+    return res.render('sign_in', {
+        title: "sign_in"
+    });
+};
--- routers/users.js
+++ routers/users.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const userController = require("../controllers/users_controller");
 
 router.get('/profile', userController.profile);
+router.get('/sign-up', userController.sign_up);
+router.get('/sign-in', userController.sign_in);
+
 module.exports = router;
 
 
