e5aac561ad0823f707f3f4eb47a0796abf43d7a5
kanishak garg <kanishakgarg8778@gmail.com>
2021-05-07 17:59:36 +0530

    create controller for sign-up the user and store detail iin database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/users');
 module.exports.profile = function (req, res) {
     return res.render('home', {
         title: "Profile"
@@ -18,12 +19,44 @@ module.exports.sign_in = function (req, res) {
     });
 };
 
-
+// creating new user (SIGN UP)
 module.exports.create = function (req, res) {
-    //TODO create the controller
-}
+    if (req.body.confirm_password != req.body.password) {
+        console.log("password not matching");
+        return res.redirect('back');
+    }
+
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) { console.log("error in finding user"); return; }
+        if (user) {
+            console.log("already exists");
+        }
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) { console.log("error in creating user with sign up"); return; }
+                return res.redirect('/users/sign-in');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    });
+};
 
 
 module.exports.createSession = function (req, res) {
-    //TODO create the controller
+    // User.findOne({ email: req.body.email }, function (err, user) {
+    //     if (err) { console.log("error finding the user in sign-in"); return }
+
+    //     if (!user) {
+    //         console.log("user not exit");
+    //         return res.redirect('back');
+    //     } else {
+    //         if (user.password == req.body.password) {
+    //             console.log("SIGNED in");
+    //             return res.redirect('/');
+    //         } else {
+    //             return res.redirect('back');
+    //         }
+    //     }
+    // });
 }
--- routers/users.js
+++ routers/users.js
@@ -8,6 +8,8 @@ router.get('/profile', userController.profile);
 router.get('/sign-up', userController.sign_up);
 router.get('/sign-in', userController.sign_in);
 
+router.post('/create', userController.create);
+router.post('/create-session', userController.createSession)
 module.exports = router;
 
 
