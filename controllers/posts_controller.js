0c6eb2349fe55ff684336184ee39a743abcae702
kanishak garg <kanishakgarg8778@gmail.com>
2021-05-10 02:12:27 +0530

    create comments schema and store comment in database

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comments');
 
 module.exports.create = function (req, res) {
     Post.create({
@@ -8,4 +9,18 @@ module.exports.create = function (req, res) {
         if (err) { console.log("error in creating the post"); return; }
         return res.redirect('back');
     });
+};
+
+
+module.exports.addComment = function (req, res) {
+    //console.log(req.body.content);
+    Comment.create({
+        content: req.body.content,
+        user: req.user._id,
+        post: req.query.id
+    }, function (err, comment) {
+        if (err) { console.log("error in creating the comment"); return; }
+        return res.redirect('back');
+    });
+
 }
--- routers/posts.js
+++ routers/posts.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const postController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthenticated, postController.create);
-
+router.post('/comment/', passport.checkAuthenticated, postController.addComment);
 module.exports = router;
