5c3cbdd1723fe54cdd1e234e924e4501e2220996
kanishak garg <kanishakgarg8778@gmail.com>
2021-05-09 23:32:27 +0530

    add SCSS

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-stratergy');
 const { mongo } = require('mongoose');
 const mongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
 
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(expressLayouts);
