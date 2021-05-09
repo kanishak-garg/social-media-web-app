aff21b852872569283ade04cc9dcc91bd8528b79
kanishak garg <kanishakgarg8778@gmail.com>
2021-05-10 01:34:29 +0530

    add the post author name using populate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,13 @@
+const Post = require('../models/posts');
+
 module.exports.home = function (req, res) {
-    return res.render('home', {
-        title: "codial"
+    //populate the user before hand check in mongoose documentarion (populate)
+    Post.find({}).populate('user').exec(function (err, posts) {
+
+        if (err) { console.log("error in finding posts"); return }
+        return res.render('home', {
+            title: "codial",
+            post_list: posts
+        });
     });
 };
