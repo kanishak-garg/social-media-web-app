3c8a9867d6cbeaef1c8992b4259aeb390ceb7081
kanishak garg <kanishakgarg8778@gmail.com>
2021-06-21 01:22:46 +0530

    make router for chat controller

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const passport = require('passport');
 const router = express.Router();
+
 const homeController = require("../controllers/home_controller");
+const chattingController = require("../controllers/chatting_controller");
 
 router.get('/', homeController.home);
+router.post('/send-message',passport.checkAuthenticated,chattingController.send_message);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
